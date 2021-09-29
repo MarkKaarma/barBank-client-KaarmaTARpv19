05937e3ca4ad6b9d07bd6e95840751fa36e23057
irinamerkulova <irina.merkulova@tthk.ee>
2021-09-29 13:13:47 +0300

    As a user I can log in and out [Delivers #179743584]

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,5 +1,15 @@
 <script>
 	export let segment;
+	import { goto, stores} from '@sapper/app';
+	import { post } from 'utils.js'
+
+	const {page, session} = stores();
+	async function logout() {
+		await post(`auth/logout`);
+		// this will trigger a redirect, because it
+		// causes the `load` function to run again
+		$session.token = null;
+	}
 </script>
 
 <style>
@@ -48,8 +58,14 @@
 	}
 </style>
 
+
 <nav>
 	<ul>
+		{#if $session.token}
+			<li><a aria-current="{segment === 'logout' ? 'page' : undefined}" href="logout" on:click|preventDefault={logout}>log out</a></li>
+			{:else}
+		<li><a aria-current="{segment === 'login' ? 'page' : undefined}" href="login">log in</a></li>
 		<li><a aria-current="{segment === 'register' ? 'page' : undefined}" href="register">register</a></li>
+			{/if}
 	</ul>
 </nav>
